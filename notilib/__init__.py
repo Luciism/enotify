9d7053eae24b445ddcfe02f0d77838b3ec19ccfe
Depleted <odepleted@gmail.com>
2023-09-29 00:12:20 +1300

    Updated __init__.py

--- notilib/__init__.py
+++ notilib/__init__.py
@@ -1,8 +1,16 @@
+"""The internal library for Enotify"""
+
+
 from .loggers.formatters import *
 from .loggers.handlers import *
 from .loggers.utils import *
 
+from .accounts import *
+from .account_manager import *
 from .database import *
+from .functions import *
+from .permissions import *
+
 
 import os
 PROJECT_PATH = os.path.abspath(f'{__file__}/../..')
