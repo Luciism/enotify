4638cb3a4e9f8dc0d39f63212d8ab9253d6a61e9
Depleted <odepleted@gmail.com>
2023-09-29 00:11:51 +1300

    Made connect method more robust and efficient

--- notilib/database.py
+++ notilib/database.py
@@ -1,3 +1,6 @@
+"""Main database connector"""
+
+
 import os
 import asyncpg
 from typing import Any
@@ -6,53 +9,42 @@ from dotenv import load_dotenv; load_dotenv()
 
 
 class Database:
-    # make sure only one instance exists
     _instance = None
+    conn: asyncpg.Connection = None
 
 
+    # make sure only one instance exists
     def __new__(cls) -> Any:
         if cls._instance is None:
             cls._instance = super(Database, cls).__new__(cls)
         return cls._instance
 
 
-    def __init__(self):
-        self.host = os.getenv('postgres_host')
-        self.port = os.getenv('postgres_port')
-        self.database = os.getenv('postgres_database')
-        self.user = os.getenv("postgres_user")
-        self.password = os.getenv('postgres_password')
-        self.conn: asyncpg.Connection = None
+    async def connect(self) -> asyncpg.Connection:
+        """
+        Connects to the database
+        :return: the `asyncpg.Connection` object of the connection (self.conn)
+        """
+        # connect if connection doesnt exist or is closed
+        if self.conn is None or self.conn.is_closed():
+            try:
+                self.conn = await asyncpg.connect(
+                    host=os.getenv('postgres_host'),
+                    port=os.getenv('postgres_port'),
+                    database=os.getenv('postgres_database'),
+                    user=os.getenv("postgres_user"),
+                    password=os.getenv('postgres_password')
+                )
+                print("Connected to PostgreSQL database!")
+            except (Exception, asyncpg.PostgresError) as error:
+                print(f"Error while connecting to PostgreSQL: {error}")
+                return
 
-
-    async def connect(self):
-        try:
-            self.conn = await asyncpg.connect(
-                host=self.host,
-                port=self.port,
-                database=self.database,
-                user=self.user,
-                password=self.password
-            )
-            print("Connected to PostgreSQL database!")
-        except (Exception, asyncpg.PostgresError) as error:
-            print(f"Error while connecting to PostgreSQL: {error}")
+        return self.conn
 
 
     async def close(self):
+        """Closes are database connection"""
         if self.conn:
             await self.conn.close()
             print("Disconnected from PostgreSQL database.")
-
-
-"""TEST"""
-
-import asyncio
-
-async def main():
-    db = Database()
-    await db.connect()
-
-    # await db.conn.execute()
-
-asyncio.run(main())
