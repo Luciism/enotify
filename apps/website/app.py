46db00b83138130fc19adcc4028cf8790cd3e226
Depleted <odepleted@gmail.com>
2023-12-23 20:03:59 +1300

    Made dedicated error handling blueprint

--- apps/website/app.py
+++ apps/website/app.py
@@ -1,20 +1,15 @@
 import os
 import logging
 
-from quart import Quart, render_template
+from quart import Quart
 from dotenv import load_dotenv; load_dotenv()
 
 from notilib import Database, setup_logging, PROJECT_PATH
-from helper import (
-    InvalidDiscordAccessTokenError,
-    UserNotLoggedInError,
-    response_msg,
-    default_page_context_data
-)
 
 from blueprints.gmail.gmail import gmail_bp
 from blueprints.discord.discord import discord_bp
 from blueprints.dashboard.dashboard import dashboard_bp
+from blueprints.error_handling.error_handling import error_handling_bp
 
 
 # -------------------------- LOGGING -------------------------- #
@@ -30,6 +25,7 @@ app.config['EXPLAIN_TEMPLATE_LOADING'] = None
 app.register_blueprint(gmail_bp)
 app.register_blueprint(discord_bp)
 app.register_blueprint(dashboard_bp)
+app.register_blueprint(error_handling_bp)
 
 
 try:
@@ -51,19 +47,6 @@ async def close_db_pool():
     await db.pool.close()
     db.pool.terminate()
 
-
-# --------------------------- ERRORS -------------------------- #
-@app.errorhandler(404)
-async def not_found(error) -> str:
-    return await render_template("404.html", data=await default_page_context_data())
-
-@app.errorhandler(InvalidDiscordAccessTokenError)
-async def invalid_discord_access_token_error_handler(error) -> dict:
-    return response_msg('invalid_discord_access_token')
-
-@app.errorhandler(UserNotLoggedInError)
-async def user_not_logged_in_error_handler(error) -> dict:
-    return response_msg('not_logged_in')
 
 # ---------------------------- RUN ---------------------------- #
 if __name__ == "__main__":
---
+++ apps/website/blueprints/error_handling/error_handling.py
@@ -0,0 +1,61 @@
+import logging
+
+from quart import Blueprint, render_template, request
+
+from helper import (
+    InvalidDiscordAccessTokenError,
+    UserNotLoggedInError,
+    response_msg,
+    default_page_context_data
+)
+
+
+logger = logging.getLogger(__name__)
+logger.info('Blueprint registered.')
+
+
+error_handling_bp = Blueprint(
+    name='error_handling',
+    import_name=__name__,
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/static/error_handling/'
+)
+
+
+
+@error_handling_bp.route('/error')
+async def error_msg_route():
+    error = request.args.get('error')
+    res_msg = response_msg(error)
+
+    default_page_data = await default_page_context_data()
+
+    # not a valid error message
+    if res_msg['message'] == error:
+        return await render_template("404.html", data=default_page_data)
+
+    return await render_template('something_went_wrong.html', data={
+        **default_page_data,
+        'text': {
+            'title': res_msg['message'],
+            'description': res_msg['description'],
+        }
+    })
+
+
+# ------------------------- EXCEPTIONS ------------------------ #
+@error_handling_bp.app_errorhandler(InvalidDiscordAccessTokenError)
+async def invalid_discord_access_token_error_handler(error) -> dict:
+    return response_msg('invalid_discord_access_token')
+
+
+@error_handling_bp.app_errorhandler(UserNotLoggedInError)
+async def user_not_logged_in_error_handler(error) -> dict:
+    return response_msg('not_logged_in')
+
+
+# ------------------------ ERROR CODES ------------------------ #
+@error_handling_bp.app_errorhandler(404)
+async def not_found(error) -> str:
+    return await render_template("404.html", data=await default_page_context_data())
