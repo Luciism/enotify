262c369e9131390f6c91f6cbbb5e4ef6a47d9b35
Depleted <odepleted@gmail.com>
2023-12-27 02:50:11 +1300

    Added tests for notilib.email_addresses

---
+++ tests/test_email_addresses.py
@@ -0,0 +1,94 @@
+import pytest
+
+from .utils import MockData, execute_in_transaction
+
+from notilib import email_addresses
+
+
+
+async def _add_email_address(
+    email_address: str,
+    webmail_service: str=MockData.webmail_service,
+    discord_id: int=MockData.discord_id
+) -> None:
+    await email_addresses.add_email_address(
+        discord_id=discord_id,
+        email_address=email_address,
+        webmail_service=webmail_service
+    )
+
+async def _remove_email_address(email_address: str):
+    await email_addresses.remove_email_address(
+        discord_id=MockData.discord_id,
+        email_address=email_address,
+        webmail_service=MockData.webmail_service
+    )
+
+@pytest.mark.asyncio
+@execute_in_transaction
+async def test_add_email_address():
+    await _add_email_address(MockData.email_address())
+
+    assert MockData.email_address() in await email_addresses.get_email_addresses(
+        discord_id=MockData.discord_id,
+        webmail_service=MockData.webmail_service
+    )
+
+
+@pytest.mark.asyncio
+@execute_in_transaction
+async def test_remove_email_address():
+    await _add_email_address(MockData.email_address())
+    await _remove_email_address(MockData.email_address())
+
+    # make sure email address was removed properly
+    assert MockData.email_address() not in await email_addresses.get_email_addresses(
+        discord_id=MockData.discord_id,
+        webmail_service=MockData.webmail_service
+    )
+
+
+@pytest.mark.asyncio
+@execute_in_transaction
+async def test_get_all_email_addresses():
+    # add 2 different email addresses
+    # TODO: use 2 different webmail services when capacity is added
+    await _add_email_address(MockData.email_address[0])
+    await _add_email_address(MockData.email_address[1])
+
+    # get both email addresses by discord id
+    all_email_addresses_data = await email_addresses.get_all_email_addresses(
+        discord_id=MockData.discord_id,
+    )
+
+    all_email_addresses = [
+        email_addr_data['email_address']
+        for email_addr_data in all_email_addresses_data
+    ]
+
+    assert MockData.email_address[0] in all_email_addresses
+    assert MockData.email_address[1] in all_email_addresses
+
+
+@pytest.mark.asyncio
+@execute_in_transaction
+async def test_email_address_to_discord_ids():
+    # add same email address under 2 different discord ids
+    await _add_email_address(
+        MockData.email_address[0],
+        webmail_service=MockData.webmail_service,
+        discord_id=MockData.discord_id
+    )
+    await _add_email_address(
+        MockData.email_address[0],
+        webmail_service=MockData.webmail_service,
+        discord_id=MockData.discord_id_2
+    )
+
+    # get both discord ids by email address
+    discord_ids = await email_addresses.email_address_to_discord_ids(
+        email_address=MockData.email_address[0],
+        webmail_service=MockData.webmail_service
+    )
+    assert MockData.discord_id in discord_ids
+    assert MockData.discord_id_2 in discord_ids
--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,6 @@
+import random
 from functools import wraps
+from typing import Literal
 
 from asyncpg import Connection
 
@@ -32,5 +34,51 @@ def execute_in_transaction(coro):
     return wrapper
 
 
+class _EmailAddress:
+    all = [
+        'Prudence.Stroman60@gmail.com',
+        'Lily.Ondricka71@gmail.com',
+        'Joanny_Treutel@yahoo.com',
+        'Barry95@hotmail.com',
+        'Kendall.Frami15@hotmail.com',
+        'Ariane.Kris@yahoo.com',
+        'Melody.Lockman84@gmail.com',
+        'Michele.Hayes71@yahoo.com',
+        'Layne_Funk@gmail.com',
+        'Cornelius84@hotmail.com',
+        'Warren73@hotmail.com',
+        'Lynn.Schneider@hotmail.com',
+        'Brandy_Nicolas@hotmail.com',
+        'Lisandro.Runolfsdottir@hotmail.com',
+        'Alberto18#amazon@yahoo.com',
+        'Markus.Pollich@gmail.com'
+    ]
+
+    def __call__(self) -> str:
+        return self.all[0]
+
+    def __str__(self) -> str:
+        return self.all[0]
+
+    def __getitem__(self, index: int) -> str:
+        return self.all[index]
+
+    def random(self) -> str:
+        return random.choice(self.all)
+
+
+
 class MockData:
-    discord_id: int = 774848780234653726
+    encryption_key: str = '28b784785df7b4a6cd557cf1d620f231'
+
+    email_address = _EmailAddress()
+
+    discord_id: Literal[774848780234653726] = 774848780234653726
+    discord_id_2: Literal[453212491884885163] = 453212491884885163
+    discord_id_3: Literal[582136343667726502] = 582136343667726502
+    discord_id_3: Literal[363502852571134156] = 363502852571134156
+
+    webmail_service: Literal['gmail'] = 'gmail'
+    webmail_service_4: Literal['yahoo'] = 'yahoo'
+    webmail_service_2: Literal['outlook'] = 'outlook'
+    webmail_service_3: Literal['hotmail'] = 'hotmail'
