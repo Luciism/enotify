b1fd6021bbd02b434853f8577ff746417c243fd6
Depleted <odepleted@gmail.com>
2023-12-28 15:20:54 +1300

    test: Added schema setup and teardown to testing sessions

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,8 @@ import pytest_asyncio
 from asyncpg import Connection
 
 from .utils import db, RollbackTransaction
+from notilib import PROJECT_PATH
+
 
 pytest_plugins = ('pytest_asyncio',)
 
@@ -37,3 +39,30 @@ async def conn():
     except RollbackTransaction:
         # logging.info('rollback transition exception raised and caught')
         pass
+
+
+def _load_sql_from_file(filename: str):
+    with open(f'{PROJECT_PATH}/schema/{filename}') as file:
+        return file.read()
+
+
+@pytest_asyncio.fixture(scope='session', autouse=True)
+async def database_schema_setup_and_teardown():
+    conn: Connection
+
+    pool = await db.connect()
+    async with pool.acquire() as conn:
+
+        # make sure schema doesnt exist beforehand
+        await conn.execute('DROP SCHEMA IF EXISTS public CASCADE')
+        await conn.execute('CREATE SCHEMA public')
+
+        await conn.execute(_load_sql_from_file('tables.sql'))
+        await conn.execute(_load_sql_from_file('functions.sql'))
+        await conn.execute(_load_sql_from_file('extensions.sql'))
+
+    yield
+
+    # remove newly created schema
+    async with pool.acquire() as conn:
+        await conn.execute('DROP SCHEMA public CASCADE')
